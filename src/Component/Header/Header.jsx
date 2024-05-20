13e446a16a4d4b41aea7910ca564fdd37346e8a9
quanh21 <giaquanganh2k4@gmail.com>
2024-05-20 20:08:51 +0700

    header

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,5 +1,61 @@
+import {
+  FaMobileScreen,
+  FaShuffle,
+  FaRegHeart,
+  FaRegUser,
+  FaChevronDown,
+} from "react-icons/fa6";
+import "./header.css";
+import React, { useState } from "react";
+
 function Header() {
-  return <header>Hello world!</header>;
+  const [country, setCountry] = useState("United States");
+  const [currency, setCurrency] = useState("USD");
+
+  return (
+    <header>
+      <div className="info">
+        <div className="settings">
+          <div className="dropdown" id="country_selector">
+            <div className="select">
+              <span className="selected">{country}</span>
+              <FaChevronDown />
+            </div>
+            <ul className="menu">
+              <li onClick={() => setCountry("United States")}>United States</li>
+              <li onClick={() => setCountry("English")}>English</li>
+              <li onClick={() => setCountry("France")}>France</li>
+            </ul>
+          </div>
+          <div className="dropdown" id="currency_selector">
+            <div className="select">
+              <span className="selected">{currency}</span>
+              <FaChevronDown />
+            </div>
+            <ul className="menu">
+              <li onClick={() => setCurrency("USD")}>USD</li>
+              <li onClick={() => setCurrency("EUR")}>EUR</li>
+              <li onClick={() => setCurrency("GBR")}>GBR</li>
+            </ul>
+          </div>
+          <div className="contact">
+            <FaMobileScreen /> 123-456-789
+          </div>
+        </div>
+        <div className="userinfo">
+          <a href="#">
+            <FaShuffle /> Compare
+          </a>
+          <a href="#">
+            <FaRegHeart /> Wishlist
+          </a>
+          <a href="#">
+            <FaRegUser /> Login
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
