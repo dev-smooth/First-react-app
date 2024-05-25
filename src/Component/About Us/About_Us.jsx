a851b7b33f6176857a0852b35a12d34d20601f7e
quanh21 <giaquanganh2k4@gmail.com>
2024-05-25 15:51:42 +0700

    about-us 2

--- src/Component/About Us/About_Us.jsx
+++ src/Component/About Us/About_Us.jsx
@@ -2,32 +2,34 @@ import "./about_us.css";
 
 const About_Us = () => {
   return (
-    <div className="session about-us">
-      <h2 className="session-title">About Us</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat
-        illo sit laboriosam, labore nulla, quidem dignissimos odio atque
-        quibusdam perferendis necessitatibus quam sed in, excepturi saepe
-        dolores totam delectus.
-      </p>
-      <p>
-        Cupiditate et eos vero corrupti quas atque dolores enim nobis sunt
-        molestiae ab, quibusdam tempora saepe odio, libero voluptatum iste
-        deleniti. Nemo tempore cupiditate odit nihil expedita facere cum
-        facilis!
-      </p>
-      <p>
-        Explicabo, ad voluptatem! Dolor mollitia repellendus necessitatibus, ex
-        reiciendis maiores consectetur. Perspiciatis facere in, quos, placeat
-        necessitatibus hic et fuga nobis debitis exercitationem eaque, illo
-        accusamus provident asperiores sequi assumenda.
-      </p>
-      <p>
-        Explicabo, ad voluptatem! Dolor mollitia repellendus necessitatibus, ex
-        reiciendis maiores consectetur. Perspiciatis facere in, quos, placeat
-        necessitatibus hic et fuga nobis debitis exercitationem eaque, illo
-        accusamus provident asperiores sequi assumenda.
-      </p>
+    <div className="session">
+      <div className="about-us">
+        <h2 className="session-title">About Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+          eaque similique quae veritatis animi provident porro expedita. Dolorum
+          suscipit possimus sit dignissimos mollitia autem iure. Eligendi
+          mollitia quibusdam quidem et quia enim sequi modi voluptatum quos id
+          accusamus voluptatem nemo ratione perferendis ab consectetur, placeat
+          dicta quae possimus cum nulla!
+        </p>
+        <p>
+          Veritatis dolores praesentium sed repudiandae odit? Ullam blanditiis
+          explicabo dicta rerum ipsum numquam perspiciatis ipsa odit, minus
+          omnis quis dolorem. Quisquam iure itaque hic maxime voluptatum animi
+          nemo blanditiis aliquid fugiat explicabo? Doloribus, quidem voluptate
+          autem quos quis omnis, sunt deserunt voluptatum enim qui quasi
+          doloremque facere eveniet, sed sint!
+        </p>
+        <p>
+          Enim quod dolore inventore nam, amet laborum et asperiores impedit
+          sed, rem eligendi repellat ad veniam? Voluptates et libero consectetur
+          ullam soluta pariatur magnam repudiandae. Quos exercitationem facere,
+          aut iusto magnam esse incidunt deleniti distinctio architecto.
+          Perferendis quasi nisi unde eum ipsum voluptatibus eius voluptatum
+          quod libero! Earum, laborum veritatis!
+        </p>
+      </div>
     </div>
   );
 };
